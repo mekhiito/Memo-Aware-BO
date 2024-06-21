833912dcc02fcfb0afbfbb8c825f966f989f5578
mekhiito <amajid.ess@gmail.com>
2024-06-21 18:49:31 +0100

    Added Logs

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 # kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader)
 exp_group=$(date '+%Y%m%d-%H%M')
 
-ACQF_ARRAY=("EEIPU")
+ACQF_ARRAY=("EEIPU" "CArBO")
 for acqf in ${ACQF_ARRAY[@]}; do
-    for trial in {1..1}; do
+    for trial in {1..10}; do
         CUDA_VISIBLE_DEVICES=0 python main.py --trial-num $trial --exp-group $exp_group --acqf $acqf &
     done;
 done;
 
-# ACQF_ARRAY=("EI" "EIPS")
-# for acqf in ${ACQF_ARRAY[@]}; do
-#     for trial in {1..10}; do
-#         CUDA_VISIBLE_DEVICES=1 python main.py --trial-num $trial --exp-group $exp_group --acqf $acqf &
-#     done;
-# done;
+ACQF_ARRAY=("EI" "EIPS")
+for acqf in ${ACQF_ARRAY[@]}; do
+    for trial in {1..10}; do
+        CUDA_VISIBLE_DEVICES=1 python main.py --trial-num $trial --exp-group $exp_group --acqf $acqf &
+    done;
+done;
 
 # ACQF_ARRAY=("CArBO" "MS_BO")
 # for acqf in ${ACQF_ARRAY[@]}; do
